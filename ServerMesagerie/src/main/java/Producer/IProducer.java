256ce33a209eb38abe27ea0c1843ec9dde777b17
Ancuta Catalin-Mihai <ancuta.catalin.mihai@gmail.com>
2021-11-21 12:16:54 +0200

    create and declare Producer

---
+++ ServerMesagerie/src/main/java/Producer/IProducer.java
@@ -0,0 +1,5 @@
+interface IProducer{
+
+    public IProducer() {}
+    public void sendMessage(String message, String user);
+}
---
+++ ServerMesagerie/src/main/java/Producer/Producer.java
@@ -0,0 +1,6 @@
+class Producer implements IProducer{
+
+    public Producer(){}
+
+    public void sendMessage(String message, String user){}
+}
