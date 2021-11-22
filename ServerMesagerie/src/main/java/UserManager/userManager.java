73137a206dca127b5d36da7e3654d556db233df2
Raul <raul.scorobete99@gmail.com>
2021-11-22 15:23:03 +0200

    added initial project structure

--- ServerMesagerie/src/main/java/UserManager/userManager.java
+++ ServerMesagerie/src/main/java/UserManager/userManager.java
@@ -19,5 +19,4 @@ class userManager implements userManagerInterface{
 
         return null;
     }
-
 }
--- ServerMesagerie/src/main/java/UserManager/userManagerInterface.java
+++ ServerMesagerie/src/main/java/UserManager/userManagerInterface.java
@@ -4,4 +4,5 @@ interface userManagerInterface{
     boolean isConnected(String user_);
     String removeUser(String user_);
     String ping(String user_);
+
 }
