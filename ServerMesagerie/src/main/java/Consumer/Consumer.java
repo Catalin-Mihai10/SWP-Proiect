86c5f5461158e7cfb924bd060935eba2e270bcbf
Ancuta Catalin-Mihai <ancuta.catalin.mihai@gmail.com>
2021-11-27 13:44:31 +0200

    imported rabbitmq dependecies

--- ServerMesagerie/src/main/java/Consumer/Consumer.java
+++ ServerMesagerie/src/main/java/Consumer/Consumer.java
@@ -1,5 +1,11 @@
 package Consumer;
 
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 class Consumer implements IConsumer{
     //private MessageHandler messageHandler;
 
--- ServerMesagerie/src/main/java/Consumer/IConsumer.java
+++ ServerMesagerie/src/main/java/Consumer/IConsumer.java
@@ -1,5 +1,8 @@
 package Consumer;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 interface IConsumer{
     public Sring getMessage();
     public void closeConnection() throws IOException;
