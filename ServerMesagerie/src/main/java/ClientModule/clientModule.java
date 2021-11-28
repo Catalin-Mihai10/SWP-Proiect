816d0e5289118e9419a83e669a91123e45bb5a3a
ancajenei <jeneianca@gmail.com>
2021-11-28 14:37:35 +0200

    changes in ClientModule after diagram changes

--- ServerMesagerie/src/main/java/ClientModule/clientModule.java
+++ ServerMesagerie/src/main/java/ClientModule/clientModule.java
@@ -2,35 +2,29 @@ package ClientModule;
 
 class clientModule implements clientModuleInterface{
     private String user;
-    private String message = "";
 
     public String getUser(){
         return this.user;
     }
 
-    public String getMessage(){
-        return this.message;
-    }
-
     public clientModule(String user_){
         this.user = user_;
+        addUser(user_);
     }
 
     @Override
     public void sendMessage(String message_, String user_){
-
+        Server.sendMessage(message_, user_);
     }
 
     @Override
-    public String addUser(String user_){
-
-        return null;
+    private void addUser(String user_){
+        Server.addUser(user_);
     }
 
     @Override
-    public String ping(String user_){
-
-        return null;
+    public boolean ping(){
+        return true;
     }
 
 }
