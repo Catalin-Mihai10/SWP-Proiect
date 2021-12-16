da6168f716c81d5d6aa1d886a8bb490128cdc163
Ancuta Catalin-Mihai <ancuta.catalin.mihai@gmail.com>
2021-12-16 18:17:20 +0200

    removed unused methods

--- ServerMesagerie/src/main/java/UserManager/UserManagerInterface.java
+++ ServerMesagerie/src/main/java/UserManager/UserManagerInterface.java
@@ -7,7 +7,5 @@ import java.nio.file.Paths;
 
 public interface UserManagerInterface {
     boolean addUser(String user_) throws IOException, ParseException, java.text.ParseException;
-    boolean removeUser(String user_) throws IOException, ParseException, ParseException, java.text.ParseException;
-    //void sendSignal(String user_, boolean signal);
-
+    boolean removeUser(String user_) throws IOException, ParseException, java.text.ParseException;
 }
